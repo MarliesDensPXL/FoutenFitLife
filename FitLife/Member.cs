8435f02c5b804eb6e7f8847bc7e3a21a5e052001
Marlies Dens <marlies.dens@pxl.be>
2026-04-14 10:55:52 +0200

    Add project files.

---
+++ FitLife/Member.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitLife
+{
+    public class Member
+    {
+        public Member(string name, double height, double weight)
+        {
+            Name = name;
+            Height = height;
+            Weight = weight;
+        }
+
+        public string Name { get; }
+        public double Height { get; }
+        public double Weight { get; }
+        public DateTime StartDate { get; }
+        public DateTime ValidUntil { get; }
+
+        private bool IsActive()
+        {
+            if (StartDate < DateTime.Today)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ActivateMemebership(DateTime startDate)
+        {
+            if (IsActive())
+            {
+                throw new Exception("Het lidmaatschap van lid {Name} kan niet geactiveerd worden omdat het is al actief is!");
+            }
+
+            StartDate = startDate;
+            ValidUntil = startDate.AddYears(1);
+        }
+
+        public void RenewMembership(int years)
+        {
+            if (!IsActive())
+            {
+                throw new Exception("Het lidmaatschap van lid {name} kan niet verlengd worden omdat dit niet actief is!");
+            }
+            ValidUntil = ValidUntil.AddYears(2);
+        }
+
+        public void DeactivateMembership(DateTime endDate)
+        {
+            if (IsActive())
+            {
+                throw new Exception("Het lidmaatschap van lid {Name} kan niet gestopt worden omdat het niet actief is!");
+            }
+
+            ValidUntil = endDate;
+        }
+    }
+}
---
+++ FitLife/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace FitLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij FitLife!");
+            Console.WriteLine("Schrijf je in via onderstaande formulier.");
+            Console.WriteLine();
+            Console.Write("Naam: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Lengte in meter: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.Write("Gewicht in kg: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Datum eerste training: ");
+            DateTime training = DateTime.Parse(Console.ReadLine());
+
+            //Print summary:
+            Console.Clear();
+            Console.Write("Bedankt {name}, controleer je gegevens nog een laatste keer:");
+            Console.WriteLine($"Naam:   \t {name}");
+            Console.WriteLine($"Lengte: \t {weight:f2}m");
+            Console.WriteLine($"Gewicht:\t {height:f2}kg");
+            Console.WriteLine($"Start:  \t {traning.ToLongDateString}");
+
+            Console.WriteLine("Druk op een toets om je lidmaatschap te activeren...");
+            Console.ReadKey(true);
+
+            Member member;
+            member.Name = name;
+            member.Height = height;
+            member.Weight = weight;
+
+            member.ActivateMembership(training);
+
+            Console.WriteLine($"Lidmaatschap succesvol geactiveerd voor {member.Name} op {member.StartDate}.");
+            Console.WriteLine("Druk op een toets om verder te gaan...");
+            Console.ReadKey(true);
+
+            bool close = false;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Welkom bij FitLife {member.name}!");
+                Console.WriteLine("Beheer hier je lidmaatschap");
+                Console.WriteLine();
+                Console.WriteLine("1. Lidmaatschap verlengen");
+                Console.WriteLine("2. Lidmaatschap stopzetten");
+                Console.Write("Selecteer de gewenste optie: ");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        member.RenewMembership();
+                        Console.WriteLine($"Proficiat {member.Name}, jouw lidmaatschap is verlengd tot {member.ValidUnit}")
+                        break;
+                    case "2":
+                        Console.Write("Datum stopzetting: ");
+                        string endDate = Console.ReadLine();
+                        member.DeactivateMembership(endDate);
+                        break;
+                    default:
+                        close = true;
+                        break;
+                }
+
+            } while (close); ;
+        }
+    }
+}
